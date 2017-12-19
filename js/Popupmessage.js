6d5ae8c131f7a8137ac97c9debd05aecc116b4c6
Adnan991 <Adnan991@githup.com>
2017-12-19 10:58:59 +0000

    Website v1
    Website v1

---
+++ js/Popupmessage.js
@@ -0,0 +1,11 @@
+function registerInterest(){
+  alert("Thank you, your interest has been registered!");
+}
+
+function submitFeedback(){
+  alert("Thank you, your feedback has been submited!");
+}
+
+function submitApplyform(){
+  alert("Thank you, your Apply form has been submited!");
+}
---
+++ js/textEnlarge.js
@@ -0,0 +1,27 @@
+function textEnlarge() {
+  document.getElementById('text-1').style.fontSize="xx-large";
+}
+function textReduce() {
+  document.getElementById('text-1').style.fontSize="15pt";
+}
+
+function textEnlarge2() {
+  document.getElementById('text-2').style.fontSize="xx-large";
+}
+function textReduce2() {
+  document.getElementById('text-2').style.fontSize="15pt";
+}
+
+function textEnlarge3() {
+  document.getElementById('text-3').style.fontSize="xx-large";
+}
+function textReduce3() {
+  document.getElementById('text-3').style.fontSize="15pt";
+}
+
+function textEnlarge4() {
+  document.getElementById('text-4').style.fontSize="xx-large";
+}
+function textReduce4() {
+  document.getElementById('text-4').style.fontSize="15pt";
+}
